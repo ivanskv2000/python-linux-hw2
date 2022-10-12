34d9e533d0c3fb0aff428b17aed1ecb56765f9cb
Ivan Skvortsov <iaskvortsov@avito.ru>
2022-10-12 19:25:09 +0300

    Black'ed the code

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -6,11 +6,13 @@ from typing import Iterable, Union
 
 class CalculatorError(Exception):
     """Base class for Calculator exceptions"""
+
     pass
 
 
 class EmptyInputError(CalculatorError):
     """Raised when an empty input is given"""
+
     def __init__(self):
         self.message = "Empty input provided."
         super().__init__(self.message)
@@ -18,24 +20,26 @@ class EmptyInputError(CalculatorError):
 
 class ParenthesesBalancingError(CalculatorError):
     """Raised when unbalanced parentheses are found in input"""
+
     def __init__(self, input_string):
         self.input_string = input_string
-        self.message = f"Matching parenthesis not found in \'{self.input_string}\'."
+        self.message = f"Matching parenthesis not found in '{self.input_string}'."
         super().__init__(self.message)
 
 
 class UnknownToken(CalculatorError):
     """Raised when an unknown token is present in math expression"""
+
     def __init__(self, token):
         self.unknown_token = token
-        self.message = f"Couldn't understand token \'{self.unknown_token}\'."
+        self.message = f"Couldn't understand token '{self.unknown_token}'."
         super().__init__(self.message)
 
 
 class IncompleteExpression(CalculatorError):
     def __init__(self, operator):
         self.operator = operator
-        self.message = f"No operand found for operator \'{self.operator}\'."
+        self.message = f"No operand found for operator '{self.operator}'."
         super().__init__(self.message)
 
 
@@ -51,19 +55,14 @@ class Calculator:
         ")": None,
     }
 
-    operators = list(
-        filter(
-            lambda x: x not in ["(", ")"],
-            precedence_levels.keys()
-            )
-        )
+    operators = list(filter(lambda x: x not in ["(", ")"], precedence_levels.keys()))
 
     funcs = {
         "+": (lambda a, b: a + b),
         "-": (lambda a, b: a - b),
         "*": (lambda a, b: a * b),
         "/": (lambda a, b: a / b),
-        "^": (lambda a, b: a ** b),
+        "^": (lambda a, b: a**b),
         "%": (lambda a, b: a % b),
     }
 
@@ -83,7 +82,7 @@ class Calculator:
             char_type = "operand"
         elif char in self.operators:
             char_type = "operator"
-        elif char in ('(', ')'):
+        elif char in ("(", ")"):
             char_type = "parenthesis"
         else:
             char_type = "other"
@@ -100,20 +99,19 @@ class Calculator:
         """
 
         token_attribution = self.char_attribution(string[0])
-        token = ''
-        for char in re.sub(r'\s+', "", string):
+        token = ""
+        for char in re.sub(r"\s+", "", string):
             new_type = self.char_attribution(char)
 
             # Case for unary minus
-            if char == '-' and \
-                    (token_attribution == 'operator' or token[-1] == '('):
+            if char == "-" and (token_attribution == "operator" or token[-1] == "("):
                 yield token
-                token = ''
-                token_attribution = 'operand'
+                token = ""
+                token_attribution = "operand"
 
             elif new_type != token_attribution:
                 yield token
-                token = ''
+                token = ""
                 token_attribution = new_type
 
             token += char
@@ -163,7 +161,10 @@ class Calculator:
                 while True:
                     if len(operator_stack) == 0:
                         break
-                    if self.precedence_levels[token] > self.precedence_levels[operator_stack[-1]]:
+                    if (
+                        self.precedence_levels[token]
+                        > self.precedence_levels[operator_stack[-1]]
+                    ):
                         break
 
                     popped = operator_stack.pop()
@@ -171,14 +172,14 @@ class Calculator:
 
                 operator_stack.append(token)
 
-            elif token == '(':
+            elif token == "(":
                 operator_stack.append(token)
 
-            elif token == ')':
+            elif token == ")":
                 while True:
                     if len(operator_stack) == 0:
-                        raise ParenthesesBalancingError(''.join(tokenized_expression))
-                    if operator_stack[-1] == '(':
+                        raise ParenthesesBalancingError("".join(tokenized_expression))
+                    if operator_stack[-1] == "(":
                         _ = operator_stack.pop()
                         break
 
@@ -190,8 +191,8 @@ class Calculator:
 
         while len(operator_stack) > 0:
             stack_popped = operator_stack.pop()
-            if stack_popped == '(':
-                raise ParenthesesBalancingError(''.join(tokenized_expression))
+            if stack_popped == "(":
+                raise ParenthesesBalancingError("".join(tokenized_expression))
 
             output.append(stack_popped)
 
@@ -226,9 +227,9 @@ class Calculator:
         Combines all methods necessary to go from input string to the answer
         """
 
-        if input_string == '':
+        if input_string == "":
             raise EmptyInputError
-        tokenized = [i for i in self.tokenize(input_string) if i != '']
+        tokenized = [i for i in self.tokenize(input_string) if i != ""]
         rpn = self.convert_to_rpn(tokenized)
         result = self.eval_rpn(rpn)
 
--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import calculator
 import argparse
 
 parser = argparse.ArgumentParser(
-    description='Simple line-input calculator',
-    epilog='''
+    description="Simple line-input calculator",
+    epilog="""
     Usage is extremely simple. Just input a valid mathematical expression and press Enter. If you want to quit, send 'q'.
-    '''
+    """,
 )
 args = parser.parse_args()
 
@@ -15,20 +15,21 @@ class bcolors:
     ANSI escape sequences (colorful terminal output)
     """
 
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    ENDC = '\033[0m'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    ENDC = "\033[0m"
 
 
 calc = calculator.Calculator()
 
-print(f"""
+print(
+    f"""
 {bcolors.HEADER}Welcome to my calculator!{bcolors.ENDC}
 Everything is simple: do math, then input 'q' to exit.
 
@@ -37,13 +38,14 @@ Supported operations: '+', '-', '*', '/', '^', '%'.
 Docs: https://github.com/ivanskv2000/python-linux-hw2
 
 
-""")
+"""
+)
 
 while True:
     query = input(f"{bcolors.BOLD}Enter math expression:{bcolors.ENDC} ")
     query = query.strip()
 
-    if query == 'q':
+    if query == "q":
         break
 
     try:
